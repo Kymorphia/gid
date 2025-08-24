5e0aff3500fa8a6a4f9f7d6cefff9f85ff2cd503
Dejan Lekić <dejan.lekic@gmail.com>
2025-08-24 18:38:56 +0100

    GMime-3 binding to D

---
+++ defs/gmime3.d
@@ -0,0 +1,8 @@
+//!gir GMime-3.0
+//!info GMime "Provides a core library and set of utilities which may be used for the creation and parsing of messages using the Multipurpose Internet Mail Extension (MIME)."
+//!info homepage https://github.com/jstedfast/gmime/
+//!info docs https://www.kymorphia.com/gid/gmime3.html
+//!info capi https://ebassi.github.io/graphene/docs/
+
+//!subdtype Object ObjectWrap
+//!subtype ssize_t size_t
